94bbd8ebc05c4cbdf7e8b174788fb0be885c77f0
Hla Thue Prue Marma <hlathuepruemarma@Hlas-MacBook-Air.local>
2025-12-11 04:55:28 +0300

    Add to navbar

--- App.jsx
+++ App.jsx
@@ -1,11 +1,14 @@
 
 import './App.css'
+import Navbar from './Navbar/Navbar'
 
 function App() {
  
 
   return (
     <>
+
+    <Navbar></Navbar>
       
     </>
   )
---
+++ Navbar/Navbar.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import "./Navbar.css"
+
+const Navbar = () => {
+  return (
+    <div>
+      <div className=" navbar bg-base-100 shadow-sm flex justify-around">
+        <div>
+          <a className="btn btn-ghost text-xl">Htp-Customer-Service</a>
+        </div>
+        <div className="flex items-center gap-5">
+          <span>Home</span>
+          <span>Faq</span>
+          <span>Changelog</span>
+          <span>Blog</span>
+          <span>Download</span>
+          <span>Contact</span>
+          <button className="btn global-bg"> <span>+</span>New Ticket</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
